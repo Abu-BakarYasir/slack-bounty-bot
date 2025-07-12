a92a6a29ce1f15c7ccb1c64c2751a2cd767d9b36
Abu Bakar Yasir <abubakarrao999@gmail.com>
2025-07-12 12:40:23 +0500

    add the corns job and corn job secrects

--- api/index.py
+++ api/index.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 from main import run_dynamic_bot
+from main import run_dynamic_bot
+import os
 
 app = Flask(__name__)
 
+
 @app.route('/run-bot')
 def run_bot():
+    # Verify CRON_SECRET
+    auth_header = request.headers.get('Authorization')
+    expected_secret = os.getenv('CRON_SECRET')
+# Allow requests without auth_header if CRON_SECRET is unset or auth matches
+    if expected_secret and auth_header and auth_header != f'Bearer {expected_secret}':
+        return 'Unauthorized', 401
     try:
         run_dynamic_bot()
         return 'Bot executed successfully!', 200
